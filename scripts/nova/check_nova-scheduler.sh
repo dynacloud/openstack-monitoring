3dfb1f1c60ac164d239eb0370a5ffdfb5c184103
rickstok <r.stokkingreef@compello.nl>
2013-10-25 15:50:40 +0200

    Update check_nova-scheduler.sh

--- scripts/nova/check_nova-scheduler.sh
+++ scripts/nova/check_nova-scheduler.sh
@@ -56,7 +56,7 @@ fi
 
 PID=$(ps -ef | grep nova-scheduler | grep python | awk {'print$2'} | head -n 1)
 
-if ! KEY=$(netstat -epta 2>/dev/null | grep $PID 2>/dev/null | grep amqp) || test -z "$PID"
+if ! KEY=$(netstat -epta 2>/dev/null | grep nova 2>/dev/null | grep amqp) || test -z "$PID"
 then
     echo "nova-scheduler is not connected to AMQP."
     exit $STATE_CRITICAL
