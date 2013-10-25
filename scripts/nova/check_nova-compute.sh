e6d0d7a98e4aa345c517654950b6d57da406abc5
rickstok <r.stokkingreef@compello.nl>
2013-10-25 15:50:53 +0200

    Update check_nova-compute.sh

--- scripts/nova/check_nova-compute.sh
+++ scripts/nova/check_nova-compute.sh
@@ -56,7 +56,7 @@ fi
 
 PID=$(ps -ef | grep nova-compute | grep python | awk {'print$2'} | head -n 1)
 
-if ! KEY=$(netstat -epta 2>/dev/null | grep $PID 2>/dev/null | grep amqp) || test -z "$PID"
+if ! KEY=$(netstat -epta 2>/dev/null | grep nova 2>/dev/null | grep amqp) || test -z "$PID"
 then
     echo "nova-compute is not connected to AMQP."
     exit $STATE_CRITICAL
