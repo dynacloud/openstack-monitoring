1311a8a653ff6d2597e3297991285db5d3a0f453
rickstok <r.stokkingreef@compello.nl>
2014-01-29 15:53:11 +0100

    neutron to quantum rename

--- scripts/neutron/check_neutron-server.sh
+++ scripts/neutron/check_neutron-server.sh
@@ -91,7 +91,7 @@ END=`date +%s`
 
 TIME=$((END-START))
 
-PID=$(ps -ef | grep neutron-server | grep python | awk {'print$2'} | head -n 1)
+PID=$(ps -ef | grep quantum-server | grep python | awk {'print$2'} | head -n 1)
 
 if test -z $PID || test -z "$NETWORKS"
 then
